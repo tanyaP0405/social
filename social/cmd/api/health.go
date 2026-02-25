28f7ee6d84faa78da8322b3d7a6cc7dfaace1af6
tanyaP0405 <139056442+tanyaP0405@users.noreply.github.com>
2026-02-25 12:09:56 +0530

    Delete social directory

--- social/cmd/api/health.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "net/http"
-
-func (app *application) healthcheck(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("OK"))
-}
--- social/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/tanya/social
-
-go 1.25.5
-
-require github.com/go-chi/chi/v5 v5.2.5 // indirect
