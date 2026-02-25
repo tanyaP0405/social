78cc3cc0f1de406fdaf05dab6f1e0d6e05cbeb5a
tanyaP0405 <139056442+tanyaP0405@users.noreply.github.com>
2026-02-25 12:09:24 +0530

    Add files via upload

---
+++ social/cmd/api/api.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type application struct {
+	config config
+}
+type config struct {
+	addr string
+}
+
+func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(60 * time.Second))
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.healthCheckHandler)
+	})
+
+	return r
+}
+func (app *application) run(mux http.Handler) error {
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 30,
+		IdleTimeout:  time.Minute,
+	}
+	log.Printf("Starting server on %s", app.config.addr)
+	return srv.ListenAndServe()
+}
